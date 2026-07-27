f86e0c37fedf6320e1416f2458631dfb365f6c4c
Tushar Kumar <tusharkumar1036@gmail.com>
2026-07-27 22:02:31 +0530

    single thread web-server

--- src/main.rs
+++ src/main.rs
@@ -60,8 +60,21 @@ fn handle_connection(mut stream: TcpStream){
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-    if request_line == "GET / HTTP/1.1" {
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
+    /* request_line == "GET / HTTP/1.1" {
         let status_line = "HTTP/1.1 200 OK";
         let contents = fs::read_to_string("hello.html").unwrap();
         let length = contents.len();
@@ -78,7 +91,7 @@ fn handle_connection(mut stream: TcpStream) {
         );
         stream.write_all(response.as_bytes()).unwrap();
     }
-    }
+    }*/
 
 
 
@@ -104,3 +117,4 @@ message-body*/
 
 
 
+//Refactoring=====
